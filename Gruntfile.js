fca243cd31183bcc808e74281b79a4c94b038ca9
Valerie Freeman <valerah7@gmail.com>
2018-01-19 15:00:09 -0600

    boiler plate

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      browserify: {
+        files: {
+          src: './javascripts/main.js',
+          dest: './dist/app.js'
+        },
+        options: {
+          transform: ['hbsfy'],
+        }
+      },
+      jshint: {
+        files: ['./javascripts/**/*.js'],
+        options: {
+          predef: ["document", "console"],
+          esnext: true,
+          globalstrict: true,
+          globals: {},
+          browserify: true
+        }
+      },
+      sass: {
+        dist: {
+          files: {
+            "./css/main.css": "./sass/main.scss"
+          }
+        }
+      },
+      watch: {
+        javascripts: {
+          files: ['./javascripts/**/*.js'],
+          tasks: ['jshint', 'browserify']
+        },
+        hbs: {
+          files: ['./templates/**/*.hbs']
+        },
+        sass: {
+          files: ["./sass/**/*.scss"],
+          tasks: ["sass"]
+        }
+      }
+    });
+    require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('default', ['jshint', 'browserify', 'watch']);
+};   
