a948b9619f57213beb17a2f46e4dac8b374ca916
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-23 15:15:27 -0600

    added getcast fn

--- javascripts/tmdb.js
+++ javascripts/tmdb.js
@@ -1,6 +1,7 @@
 "use strict";
 let $ = require("jquery");
 let apiKey = require("./apiKey");
+const _ = require("lodash");
 
 // promises TMDb search results for ${input}
 module.exports.getMovies = (input) => {
@@ -15,6 +16,21 @@ module.exports.getMovies = (input) => {
     });
 };
 
+const getCast = movieId => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
+        })
+            .done(response => resolve(response.cast))
+            .fail(error => reject(error));
+    });
+};
 
-
-
+module.exports.getCastList = movieId => {
+    return new Promise((resolve, reject) => {
+        getCast(movieId).then(cast => {
+            let names = cast.map(castMember => castMember.name);
+            return names.slice(0,3).join(", ");
+        });
+    });
+};
