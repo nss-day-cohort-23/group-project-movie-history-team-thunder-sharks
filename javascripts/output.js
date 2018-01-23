2ffcaa0f63cf927e7141eec9e963df216b1d6f0f
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-23 09:36:08 -0600

    trying to checkout another branch

--- javascripts/output.js
+++ javascripts/output.js
@@ -9,13 +9,6 @@ module.exports.outputMovies = (formattedMovies) =>{
         $('.movieContainer').append(
         movieCard(formattedMovies[i]));
         
-        // `<div class="movieCard"> 
-        // <h2>${formattedMovies[i].title}</h2>
-        // <h7></h7>
-        // <br> 
-        // <img src="https://image.tmdb.org/t/p/w500${formattedMovies[i].poster}" class="moviePoster" alt="No Movie Poster Available" width='260px' height='370px'>
-        // <br>
-        // <h2>${formattedMovies[i].title}</h2>
-        // </div>`
+       
     }
 };
